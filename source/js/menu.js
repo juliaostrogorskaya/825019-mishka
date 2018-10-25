87c8cc3cd1a74877d6d9aefbdc6da57ad82572c9
juliaostrogorskaya <juliaostrogorskaya@gmail.com>
2018-10-26 03:51:38 +0700

    fixx

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,7 +1,5 @@
 var navMain = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".main-nav__toggle");
-
-navMain.classList.remove("main-nav--nojs");
+var navToggle = navMain.querySelector(".main-nav__toggle");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
