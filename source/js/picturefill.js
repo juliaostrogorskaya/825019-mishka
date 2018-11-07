55695bd8d06f3a1e8381825e01c8b987cb7f2db3
juliaostrogorskaya <juliaostrogorskaya@gmail.com>
2018-11-08 05:31:57 +0700

    avto

--- source/js/picturefill.js
+++ source/js/picturefill.js
@@ -1,3 +1,4 @@
+/* stylelint-disable*/
 /*! picturefill - v3.0.2 - 2016-02-12
  * https://scottjehl.github.io/picturefill/
  * Copyright (c) 2016 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT
@@ -1542,3 +1543,5 @@
 	}
 
 } )( window, document );
+
+/* stylelint-enable */
